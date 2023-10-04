8b6c102725a564e0721b90cf5df1826f5592c199
Ubuntu <ubuntu@ip-172-31-39-28.ap-northeast-2.compute.internal>
2023-10-04 05:42:16 +0000

    ShortsComponent fixed

--- src/components/ShortsComponent.js
+++ src/components/ShortsComponent.js
@@ -1,9 +1,21 @@
 import React from "react"
+import styles from "../css/AboutMain.module.css"
 
 const ShortsComponent = () => {
     return (
         <>
-            <h1>메롱 이건 쇼츠 페이지랍니다!</h1>
+            <div id={styles.shortsBox}>
+                메롱 메롱 메롱
+                스테이지어스
+            </div>
+            <div id={styles.settings}>
+                <button class={styles.settingsItem}>좋아요</button>
+                <button class={styles.settingsItem}>싫어요</button>
+                <button class={styles.settingsItem}>댓글</button>
+                <button class={styles.settingsItem}>공유</button>
+                <button class={styles.settingsItem}>기타</button>
+                <button class={styles.settingsItem}>프로필</button>
+            </div>
         </>
     )
 }
